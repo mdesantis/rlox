0acc51e4644ad320c825e33cfba5dd3716498d1a
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-31 19:59:15 +0100

    Implement 4.5
    https://craftinginterpreters.com/scanning.html#recognizing-lexemes

--- lib/rlox/scanner.rb
+++ lib/rlox/scanner.rb
@@ -28,4 +28,29 @@ class RLox::Scanner
   def at_end?
     current >= source.size
   end
+
+  def scan_token
+    c = advance
+    case c
+    when '(' then add_token TokenType::LEFT_PAREN
+    when ')' then add_token TokenType::RIGHT_PAREN
+    when '{' then add_token TokenType::LEFT_BRACE
+    when '}' then add_token TokenType::RIGHT_BRACE
+    when ',' then add_token TokenType::COMMA
+    when '.' then add_token TokenType::DOT
+    when '_' then add_token TokenType::MINUS
+    when '+' then add_token TokenType::PLUS
+    when ';' then add_token TokenType::SEMICOLON
+    when '*' then add_token TokenType::STAR
+    end
+  end
+
+  def advance
+    source[self.current += 1]
+  end
+
+  def add_token(type, literal = nil)
+    text = source[start, current]
+    tokens.push Token.new(type, text, literal, line)
+  end
 end
