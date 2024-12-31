7015dc1e8f188d10b9c6386b3980b88cd3147f06
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-31 18:51:10 +0100

    Implement 4.1
    https://craftinginterpreters.com/scanning.html#the-interpreter-framework

--- lib/rlox.rb
+++ lib/rlox.rb
@@ -3,6 +3,39 @@
 require_relative 'rlox/version'
 
 module RLox
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  def main
+    case ARGV.size
+    when 1 then run_file ARGV[0]
+    when 0 then run_prompt
+    else
+      warn 'Usage: rlox [script]'
+      exit 64
+    end
+  end
+
+  def run_file(path)
+    run File.read path
+  end
+
+  def run_prompt
+    loop do
+      print '> '
+
+      break if ARGF.eof?
+
+      run ARGF.readline
+    end
+  end
+
+  def run(source)
+    scanner = Scanner.new(source)
+    tokens = scanner.scan_tokens
+
+    # For now, just print the tokens.
+    tokens.each do |token|
+      puts token
+    end
+  end
 end
