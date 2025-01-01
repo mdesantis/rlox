eea2536806ce0c48b0bcc5ff3b540775faf023dc
Maurizio De Santis <desantis.maurizio@gmail.com>
2025-01-01 10:40:25 +0100

    Implement 4.5.2
    https://craftinginterpreters.com/scanning.html#operators

--- lib/rlox/scanner.rb
+++ lib/rlox/scanner.rb
@@ -1,57 +1,75 @@
 # frozen_string_literal: true
 
-class RLox::Scanner
-  def initialize(source)
-    @source = source
-    @tokens = []
-    @start = 0
-    @current = 0
-    @line = 1
-  end
+class RLox
+  class Scanner
+    include TokenType
 
-  def scan_tokens
-    until at_end?
-      # We are at the beginning of the next lexeme.
-      self.start = current
-      scan_token
+    def initialize(source)
+      @source = source
+      @tokens = []
+      @start = 0
+      @current = 0
+      @line = 1
     end
 
-    tokens.push RLox::Token.new(RLox::TokenType::EOF, '', nil, line)
-    tokens
-  end
+    def scan_tokens
+      until at_end?
+        # We are at the beginning of the next lexeme.
+        self.start = current
+        scan_token
+      end
 
-  private
-
-  attr_reader :source, :tokens
-  attr_accessor :start, :current, :line
-
-  def at_end?
-    current >= source.size
-  end
-
-  def scan_token
-    c = advance
-    case c
-    when '(' then add_token RLox::TokenType::LEFT_PAREN
-    when ')' then add_token RLox::TokenType::RIGHT_PAREN
-    when '{' then add_token RLox::TokenType::LEFT_BRACE
-    when '}' then add_token RLox::TokenType::RIGHT_BRACE
-    when ',' then add_token RLox::TokenType::COMMA
-    when '.' then add_token RLox::TokenType::DOT
-    when '_' then add_token RLox::TokenType::MINUS
-    when '+' then add_token RLox::TokenType::PLUS
-    when ';' then add_token RLox::TokenType::SEMICOLON
-    when '*' then add_token RLox::TokenType::STAR
-    else RLox.error line, 'Unexpected character.'
+      tokens.push Token.new(EOF, '', nil, line)
+      tokens
     end
-  end
 
-  def advance
-    source[self.current += 1]
-  end
+    private
 
-  def add_token(type, literal = nil)
-    text = source[start, current]
-    tokens.push RLox::Token.new(type, text, literal, line)
+    attr_reader :source, :tokens
+    attr_accessor :start, :current, :line
+
+    def at_end?
+      current >= source.size
+    end
+
+    def scan_token
+      c = advance
+      case c
+      when '(' then add_token LEFT_PAREN
+      when ')' then add_token RIGHT_PAREN
+      when '{' then add_token LEFT_BRACE
+      when '}' then add_token RIGHT_BRACE
+      when ',' then add_token COMMA
+      when '.' then add_token DOT
+      when '_' then add_token MINUS
+      when '+' then add_token PLUS
+      when ';' then add_token SEMICOLON
+      when '*' then add_token STAR
+      when '!' then add_token match?('=') ? BANG_EQUAL : BANG
+      when '=' then add_token match?('=') ? EQUAL_EQUAL : EQUAL
+      when '<' then add_token match?('=') ? LESS_EQUAL : LESS
+      when '>' then add_token match?('=') ? GREATER_EQUAL : GREATER
+      else RLox.error line, 'Unexpected character.'
+      end
+    end
+
+    def advance
+      current = self.current
+      self.current += 1
+      source[current]
+    end
+
+    def add_token(type, literal = nil)
+      text = source[start...current]
+      tokens.push Token.new(type, text, literal, line)
+    end
+
+    def match?(expected)
+      return false if at_end?
+      return false if source[current] != expected
+
+      self.current += 1
+      true
+    end
   end
 end
