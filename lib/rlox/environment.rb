161b815b3b42205604e266d3e8518060fbc9fb57
Maurizio De Santis <desantis.maurizio@gmail.com>
2025-01-02 22:54:55 +0100

    Implement 8.3
    https://craftinginterpreters.com/statements-and-state.html#environments

---
+++ lib/rlox/environment.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class RLox
+  class Environment
+    def initialize
+      @values = values
+    end
+
+    def define(name, value)
+      values[name] = value
+    end
+
+    def get(name)
+      values.fetch name.lexeme
+    rescue KeyError
+      raise RLox::RuntimeError.new name, "Undefined variable '#{name.lexeme}'."
+    end
+
+    private
+
+    attr_accessor :values
+  end
+end
