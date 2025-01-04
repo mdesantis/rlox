526fa51974c181338b91923fa14f29c050e1e73a
Maurizio De Santis <desantis.maurizio@gmail.com>
2025-01-04 11:33:00 +0100

    Implement 11.3.1
    https://craftinginterpreters.com/resolving-and-binding.html#resolving-blocks

--- lib/rlox/resolver.rb
+++ lib/rlox/resolver.rb
@@ -4,10 +4,41 @@ class RLox
   class Resolver
     def initialize(interpreter)
       @interpreter = interpreter
+      @scopes = []
+    end
+
+    def resolve(statements)
+      statements.each do |statement|
+        resolve_statement statement
+      end
+    end
+
+    def visit_block_stmt(stmt)
+      begin_scope
+      resolve stmt.statements
+      end_scope
+
+      nil
     end
 
     private
 
-    attr_reader :interpreter
+    attr_reader :interpreter, :scopes
+
+    def resolve_statement(stmt)
+      stmt.accept self
+    end
+
+    def resolve_expression(expr)
+      expr.accept self
+    end
+
+    def begin_scope
+      scopes.push({})
+    end
+
+    def end_scope
+      scopes.pop
+    end
   end
 end
